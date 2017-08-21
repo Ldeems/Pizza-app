acc466c411ecb97736326fe966b0f342d335fd1d
Ldeems <luke.deems@yahoo.com>
2017-08-21 13:05:22 -0400

    got order name input working gonna update delploy

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ enable :sessions
 get '/' do
 	erb :welcome
 end
+
+post '/name' do
+	session[:ordername] = params[:ordername]
+	redirect '/placeorder?'
+end	
+
+get '/placeorder' do
+	p "#{session[:ordername]} look here"
+	erb :placeorder
+end	
