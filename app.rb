8d5889570a5b314c6948f6747c2c125ae09d6192
Ldeems <luke.deems@yahoo.com>
2017-08-28 14:22:21 -0400

    minor fixing uodate deploy

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ enable :sessions
 
 get '/' do
 	erb :welcome
-	session[:veggie] = nil
-	session[:meat] = nil
 end
 
 post '/name' do
@@ -40,7 +38,7 @@ get '/placeorder' do
 end	
 
 post '/placeorder' do
-
+	
 	pric = 0
 	session[:size] = params[:size]
 	if session[:size] == "featherwieght"
